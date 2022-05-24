43b3a0d4b28dea022c279c5837128990b754ce1b
chris <chris_z_su@163.com>
2022-05-24 10:10:56 +0800

    springboot日志级别控制：注解方式

--- springboot_11_log/src/main/java/com/shubao/controller/BookController.java
+++ springboot_11_log/src/main/java/com/shubao/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.shubao.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,22 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2022-05-11 19:07
  * @since JDK1.8
  **/
+@Slf4j
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
     //创建记录日志的对象
-    private static Logger logger = LoggerFactory.getLogger(BookController.class);
+    // private static Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @GetMapping
     public String getById() {
         System.out.println("SpringBoot is running...");
 
         //记录日志
-        logger.info("log info...");
-        logger.debug("log debug...");
-        logger.error("log error...");
-        logger.warn("log warn...");
+        log.info("log info...");
+        log.debug("log debug...");
+        log.error("log error...");
+        log.warn("log warn...");
 
         return "Hello Books";
     }
